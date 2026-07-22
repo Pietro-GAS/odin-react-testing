bff2f0e776785e7ff782a03714eb2a87695d1d9b
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-07-22 06:58:20 +0000

    tests with mock functions

---
+++ src/tests/CustomButton.test.jsx
@@ -0,0 +1,34 @@
+import { vi, describe, it, test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CustomButton from "../CustomButton";
+
+describe("CustomButton", () => {
+    it("should render a button with the test 'Click me'", () => {
+        render(<CustomButton onClick={() => {}} />);
+
+        const button = screen.getByRole("button", { name: "Click me" });
+
+        expect(button).toBeInTheDocument();
+    });
+
+    it("should call the onClick function when clicked", async () => {
+        const onClick = vi.fn();
+        const user = userEvent.setup();
+
+        render(<CustomButton onClick={onClick} />);
+        
+        const button = screen.getByRole("button", { name: "Click me" });
+        await user.click(button);
+
+        expect(onClick).toHaveBeenCalled();
+    });
+
+    it("should not call the onClick function when not clicked", () => {
+        const onClick = vi.fn();
+        
+        render(<CustomButton onClick={onClick} />);
+        
+        expect(onClick).not.toHaveBeenCalled();
+    })
+});
